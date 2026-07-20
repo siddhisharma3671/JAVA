dffaa2fa27a986e171321614a90081620fbcd79b
Siddhi Sharma <siddhisharma3671@gmail.com>
2026-07-20 13:35:31 +0530

    Adding JAVA CODES

---
+++ SetBits.java
@@ -0,0 +1,18 @@
+import javax.swing.plaf.synth.SynthPasswordFieldUI;
+
+public class SetBits {
+    public static void main(String[] args) {
+        int n =45;
+        System.out.println(Integer.toBinaryString(n));
+        System.out.println(setBits(n));
+    }
+    private static int setBits(int n){
+        int count =0;
+        while(n>0){
+            count++;
+            n-=(n&-n);
+        }
+        return count;
+    }
+    
+}
